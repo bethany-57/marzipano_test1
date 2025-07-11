6428d65391edc87272bdf9bb3022c7364d07cbde
bethany-57 <bethanyroseseaman@gmail.com>
2025-07-11 14:38:55 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,118 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ex-arable-neat",
+      "name": "ex-arable neat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.228104569955013,
+          "pitch": -0.1968763879492368,
+          "rotation": 0,
+          "target": "1-cows-close"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5051044780454035,
+          "pitch": 0.0019938594478858818,
+          "title": "Flowers in neighboring fields",
+          "text": "<img src=\"https://i.postimg.cc/x85rSYjh/DSC-0021.jpg\">"
+        },
+        {
+          "yaw": -1.840046522254779,
+          "pitch": -0.3156837132730228,
+          "title": "320x240",
+          "text": "<img src=\"https://i.postimg.cc/KYj8qqfm/DSC-0024.jpg\">"
+        },
+        {
+          "yaw": -2.3811160199550443,
+          "pitch": -0.1492813772824988,
+          "title": "Title",
+          "text": "<img src=\"https://i.postimg.cc/wM8xB6pB/DSC-0029.jpg\">"
+        },
+        {
+          "yaw": -1.8482092320120458,
+          "pitch": -0.44226824474311854,
+          "title": "150x112",
+          "text": "<img src=\"https://i.postimg.cc/s2gMFDjd/DSC-0024.jpg\"><div><br></div>"
+        },
+        {
+          "yaw": -1.8399635287410057,
+          "pitch": -0.17395807473675617,
+          "title": "no resize",
+          "text": "<img src=\"https://i.postimg.cc/65K5J5cj/DSC-0024.jpg\">"
+        }
+      ]
+    },
+    {
+      "id": "1-cows-close",
+      "name": "cows close",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6340521569010882,
+          "pitch": -0.10952836117597897,
+          "rotation": 0,
+          "target": "0-ex-arable-neat"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
